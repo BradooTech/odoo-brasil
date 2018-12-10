9468b7697d4da0cbf0d2bdf4ad8637d1b882e2ee
Felipe Goes <fgoesoliveira@gmail.com>
2018-12-10 17:42:12 -0200

    [FIX] - Ajustes gerais

--- br_nfe/models/payment_mode.py
+++ br_nfe/models/payment_mode.py
@@ -7,6 +7,7 @@ from odoo import fields, models
 class PaymentMode(models.Model):
     _inherit = "payment.mode"
 
+    # Opção '14' foi alterada de 'Duplicata Mercantil' para 'Boleto'
     tipo_pagamento = fields.Selection(
         [('01', 'Dinheiro'),
          ('02', 'Cheque'),
@@ -17,7 +18,7 @@ class PaymentMode(models.Model):
          ('11', 'Vale Refeição'),
          ('12', 'Vale Presente'),
          ('13', 'Vale Combustível'),
-         ('14', 'Duplicata Mercantil'),
+         ('14', 'Boleto'),
          ('90', 'Sem pagamento'),
          ('99', 'Outros')],
         string="Forma de Pagamento", default="14")
